15ecd7c84e7abae17e74e6b3a4f7d534b121e67a
KenayOG <angel1202a@gmail.com>
2024-07-29 15:24:26 -0600

    Obtener el detalle del componente por ID

--- src/app/inventario/detalle-componente/detalle-componente.component.ts
+++ src/app/inventario/detalle-componente/detalle-componente.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ComponentDetail } from '../../interfaces/component-detail';
+import { ComponentInventory } from '../../interfaces/component-inventory';
+import { InventarioComponenteService } from '../../services/inventario-componente.service';
 
 @Component({
   selector: 'app-detalle-componente',
@@ -8,14 +10,24 @@ import { ComponentDetail } from '../../interfaces/component-detail';
   styleUrl: './detalle-componente.component.css',
 })
 export class DetalleComponenteComponent implements OnInit {
-  componenteId: string | null = '';
-  constructor(private route: ActivatedRoute) {}
+  componentDetail: ComponentInventory | undefined;
+  nombreComponente: string = '';
+  constructor(
+    private route: ActivatedRoute,
+    private inventarioService: InventarioComponenteService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      this.componenteId = params.get('id');
-    });
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.inventarioService.getComponentePorId(id).subscribe(
+      (component) => {
+        this.componentDetail = component;
+        this.nombreComponente =
+          this.componentDetail?.nombre || 'Nombre del Componente no encontrado';
+      },
+      (error) => {
+        console.error('Error al obtener el detalle del componente', error);
+      }
+    );
   }
-
-  componentDetail: ComponentDetail[] = [];
 }
