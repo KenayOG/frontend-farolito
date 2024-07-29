ed0707918c742096cc158f4382e51e752a433f82
KenayOG <angel1202a@gmail.com>
2024-07-29 15:51:50 -0600

    Modificacion de propiedades de interfaz de detalles del inventario lampara

--- src/app/interfaces/lamp-detail.ts
+++ src/app/interfaces/lamp-detail.ts
@@ -1,10 +1,7 @@
-export interface LampDetail{
+export interface LampDetail {
   id: number;
-  name: string;
-  quantity: number;
-  price: number;
-  lote: string;
-  date: string;
-  productionCost: number;
-  createdBy: string;
+  fechaProduccion: string;
+  usuario: string;
+  cantidad: number;
+  precio: number;
 }
