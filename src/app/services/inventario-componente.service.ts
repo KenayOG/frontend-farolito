55627251ec0e320a6d18666adadee627373723eb
KenayOG <angel1202a@gmail.com>
2024-07-29 15:22:24 -0600

    Metodo para obtener el componente por ID

--- src/app/services/inventario-componente.service.ts
+++ src/app/services/inventario-componente.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ComponentInventory } from '../interfaces/component-inventory';
 
 @Injectable({
@@ -16,6 +17,13 @@ export class InventarioComponenteService {
   getInventarioComponente(): Observable<ComponentInventory[]> {
     return this._http.get<ComponentInventory[]>(
       `${this.apiUrl}/inventario-componentes`
+    );
+  }
+
+  // Metodo para obtener el componente por ID.
+  getComponentePorId(id: number): Observable<ComponentInventory | undefined> {
+    return this.getInventarioComponente().pipe(
+      map((componentes) => componentes.find((component) => component.id === id))
     );
   }
 }
