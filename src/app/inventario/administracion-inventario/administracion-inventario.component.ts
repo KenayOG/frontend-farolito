c137e95ed927a3147fe51232fea691010453d76d
KenayOG <angel1202a@gmail.com>
2024-07-29 14:10:02 -0600

    Limpieza de datos estaticos de inventarios, y carga de inventarios por metodo get

--- src/app/inventario/administracion-inventario/administracion-inventario.component.ts
+++ src/app/inventario/administracion-inventario/administracion-inventario.component.ts
@@ -1,124 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ComponentInventory } from '../../interfaces/component-inventory';
 import { LampInventory } from '../../interfaces/lamp-inventory';
+import { InventarioComponenteService } from '../../services/inventario-componente.service';
+import { HttpClientModule } from '@angular/common/http';
+import { InventarioLamparaService } from '../../services/inventario-lampara.service';
 
 @Component({
   selector: 'app-administracion-inventario',
   templateUrl: './administracion-inventario.component.html',
   styleUrl: './administracion-inventario.component.css',
 })
-export class AdministracionInventarioComponent {
-  componentesInventory: ComponentInventory[] = [
-    {
-      id: 1,
-      item: 'Cables',
-      quantity: 128,
-      price: 12.5,
-    },
-    {
-      id: 2,
-      item: 'Bateria',
-      quantity: 1,
-      price: 10.6,
-    },
-    {
-      id: 3,
-      item: 'Bombilla',
-      quantity: 12,
-      price: 15,
-    },
-    {
-      id: 4,
-      item: 'Interruptor',
-      quantity: 24,
-      price: 21,
-    },
-    {
-      id: 5,
-      item: 'Regulador',
-      quantity: 5,
-      price: 25,
-    },
-    {
-      id: 6,
-      item: 'Metal',
-      quantity: 10,
-      price: 5.5,
-    },
-    {
-      id: 7,
-      item: 'Luz Led',
-      quantity: 70,
-      price: 12,
-    },
-    {
-      id: 8,
-      item: 'Ceramica',
-      quantity: 55,
-      price: 11.7,
-    },
-    {
-      id: 9,
-      item: 'Enchufe',
-      quantity: 10,
-      price: 35,
-    },
-    {
-      id: 10,
-      item: 'Chips',
-      quantity: 40,
-      price: 50,
-    },
-    {
-      id: 11,
-      item: 'Transformador',
-      quantity: 35,
-      price: 90,
-    },
-    {
-      id: 12,
-      item: 'Casquillo',
-      quantity: 30,
-      price: 120,
-    },
-  ];
+export class AdministracionInventarioComponent implements OnInit {
+  componentesInventory: ComponentInventory[] = [];
 
-  lampsInventory: LampInventory[] = [
-    {
-      id: 1,
-      name: 'Lampara Moderna',
-      quantity: 30,
-      price: 120,
-    },
-    {
-      id: 2,
-      name: 'Lampara de Piso',
-      quantity: 30,
-      price: 120,
-    },
-    {
-      id: 3,
-      name: 'Lampara de Techo',
-      quantity: 30,
-      price: 120,
-    },
-    {
-      id: 4,
-      name: 'Lamparas Colgantes',
-      quantity: 30,
-      price: 120,
-    },
-    {
-      id: 5,
-      name: 'Lampara de Mesa',
-      quantity: 30,
-      price: 120,
-    },
-    {
-      id: 6,
-      name: 'Lampara Exteriores',
-      quantity: 30,
-      price: 120,
-    },
-  ];
+  lampsInventory: LampInventory[] = [];
+
+  constructor(
+    private inventarioComponenteService: InventarioComponenteService,
+    private inventarioLamparaService: InventarioLamparaService
+  ) {}
+
+  ngOnInit(): void {
+    this.inventarioComponenteService
+      .getInventarioComponente()
+      .subscribe((data) => {
+        this.componentesInventory = data;
+      });
+    this.inventarioLamparaService.getInventarioLampara().subscribe((data) => {
+      this.lampsInventory = data;
+    });
+  }
 }
