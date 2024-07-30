272ecea508c165e4dc239487a4fbf57e7bc50700
KenayOG <angel1202a@gmail.com>
2024-07-30 16:47:47 -0600

    Eliminacion de datos estaticos de produccion

--- src/app/produccion/pedidos-produccion/pedidos-produccion.component.ts
+++ src/app/produccion/pedidos-produccion/pedidos-produccion.component.ts
@@ -7,41 +7,5 @@ import { Production } from '../../interfaces/production';
   styleUrl: './pedidos-produccion.component.css',
 })
 export class PedidosProduccionComponent {
-  productions: Production[] = [
-    {
-      id: 1,
-      employee: 'Sergio de Jesus Salazar Cabrera',
-      date: '2024-02-22',
-      orderOf: 'Lampara Moderna',
-      quantity: 12,
-    },
-    {
-      id: 2,
-      employee: 'Adrian Dario Bravo Luna',
-      date: '2023-05-08',
-      orderOf: 'Lampara de Mesa',
-      quantity: 12,
-    },
-    {
-      id: 3,
-      employee: 'Jose Angel Ramirez Almeida',
-      date: '2023-01-12',
-      orderOf: 'Lampara de Techo',
-      quantity: 12,
-    },
-    {
-      id: 4,
-      employee: 'Angel Eduardo Juarez Alvizo',
-      date: '2023-09-28',
-      orderOf: 'Lamparas Colgantes',
-      quantity: 12,
-    },
-    {
-      id: 5,
-      employee: 'Alexa Guerrero Lopez',
-      date: '2023-06-26',
-      orderOf: 'Lampara Exteriores',
-      quantity: 12,
-    },
-  ];
+  productions: Production[] = [];
 }
