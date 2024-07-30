60c1f174b6f96fd1d46c6c74c5242ad71b44c8e1
KenayOG <angel1202a@gmail.com>
2024-07-29 23:15:52 -0600

    Cambios incorporados para obtener los componentes disponibles en formulario de creacion de recetas

--- src/app/recetas/administracion-recetas/administracion-recetas.component.ts
+++ src/app/recetas/administracion-recetas/administracion-recetas.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
 import { RecetasService } from '../../services/recetas.service';
 import { ComponenteRecipe } from '../../interfaces/component-recipe';
+import { Componente } from '../../interfaces/components';
+import { CatalogoComponentesService } from '../../services/catalogo-componentes.service';
 
 @Component({
   selector: 'app-administracion-recetas',
@@ -11,10 +13,14 @@ import { ComponenteRecipe } from '../../interfaces/component-recipe';
 export class AdministracionRecetasComponent implements OnInit {
   recipes: Recipe[] = [];
   filtradosComponentes: { [key: number]: ComponenteRecipe[] } = {};
+  listaComponentes: Componente[] = [];
 
   baseUrl: string = 'assets/images/';
   imageUrls: { [key: number]: string } = {};
-  constructor(private recetasService: RecetasService) {}
+  constructor(
+    private recetasService: RecetasService,
+    private componentesListaService: CatalogoComponentesService
+  ) {}
 
   ngOnInit(): void {
     this.recetasService.getRecetas().subscribe((data) => {
@@ -23,6 +29,10 @@ export class AdministracionRecetasComponent implements OnInit {
       this.recipes.forEach((recipe) => {
         this.imageUrls[recipe.id] = `${this.baseUrl}${recipe.imagen}`;
       });
+    });
+
+    this.componentesListaService.getCatalogoComponentes().subscribe((data) => {
+      this.listaComponentes = data;
     });
   }
 
