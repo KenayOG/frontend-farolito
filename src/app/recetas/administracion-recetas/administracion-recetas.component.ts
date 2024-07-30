df42a223c175b84bf5ac7746c31c5cb994a7651c
KenayOG <angel1202a@gmail.com>
2024-07-29 22:14:56 -0600

    Eliminacion de datos estaticos de recetas y componentes y creacion de filtros para componentes y recetas

--- src/app/recetas/administracion-recetas/administracion-recetas.component.ts
+++ src/app/recetas/administracion-recetas/administracion-recetas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
-import { Componente } from '../../interfaces/components';
+import { RecetasService } from '../../services/recetas.service';
+import { ComponenteRecipe } from '../../interfaces/component-recipe';
 
 @Component({
   selector: 'app-administracion-recetas',
@@ -8,124 +9,31 @@ import { Componente } from '../../interfaces/components';
   styleUrl: './administracion-recetas.component.css',
 })
 export class AdministracionRecetasComponent implements OnInit {
-  components: Componente[] = [
-    {
-      id: 1,
-      name: 'Cables',
-    },
-    {
-      id: 2,
-      name: 'Bateria',
-    },
-    {
-      id: 3,
-      name: 'Bombilla',
-    },
-    {
-      id: 4,
-      name: 'Interruptor',
-    },
-    {
-      id: 5,
-      name: 'Regulador',
-    },
-    {
-      id: 1,
-      name: 'Cables',
-    },
-    {
-      id: 2,
-      name: 'Bateria',
-    },
-    {
-      id: 3,
-      name: 'Bombilla',
-    },
-    {
-      id: 4,
-      name: 'Interruptor',
-    },
-    {
-      id: 5,
-      name: 'Regulador',
-    },
-  ];
-  recipes: Recipe[] = [
-    {
-      id: 1,
-      name: 'Lampara Moderna',
-      components: [
-        'Elemento 1',
-        'Elemento 2',
-        'Elemento 3',
-        'Elemento 4',
-        'Elemento 5',
-      ],
-      image: 'assets/images/lampara-moderna.webp',
-    },
-    {
-      id: 2,
-      name: 'Lampara Colgante',
-      components: [
-        'Elemento 1',
-        'Elemento 2',
-        'Elemento 3',
-        'Elemento 4',
-        'Elemento 5',
-      ],
-      image: 'assets/images/lampara-colgante.webp',
-    },
-    {
-      id: 3,
-      name: 'Lampara Exteriores',
-      components: [
-        'Elemento 1',
-        'Elemento 2',
-        'Elemento 3',
-        'Elemento 4',
-        'Elemento 5',
-      ],
-      image: 'assets/images/lampara-exteriores.webp',
-    },
-    {
-      id: 4,
-      name: 'Lampara Mesa',
-      components: [
-        'Elemento 1',
-        'Elemento 2',
-        'Elemento 3',
-        'Elemento 4',
-        'Elemento 5',
-      ],
-      image: 'assets/images/lampara-mesa.webp',
-    },
-    {
-      id: 5,
-      name: 'Lampara de Piso',
-      components: [
-        'Elemento 1',
-        'Elemento 2',
-        'Elemento 3',
-        'Elemento 4',
-        'Elemento 5',
-      ],
-      image: 'assets/images/lampara-pie.webp',
-    },
-    {
-      id: 6,
-      name: 'Lampara de Techo',
-      components: [
-        'Elemento 1',
-        'Elemento 2',
-        'Elemento 3',
-        'Elemento 4',
-        'Elemento 5',
-      ],
-      image: 'assets/images/lampara-techo.webp',
-    },
-  ];
+  recipes: Recipe[] = [];
+  filtradosComponentes: { [key: number]: ComponenteRecipe[] } = {};
 
-  constructor() {}
+  baseUrl: string = 'assets/images/';
+  imageUrls: { [key: number]: string } = {};
+  constructor(private recetasService: RecetasService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recetasService.getRecetas().subscribe((data) => {
+      this.recipes = data;
+      this.filtrarComponentes();
+      this.recipes.forEach((recipe) => {
+        this.imageUrls[recipe.id] = `${this.baseUrl}${recipe.imagen}`;
+      });
+    });
+  }
+
+  filtrarComponentes(): void {
+    this.filtradosComponentes = {};
+    this.recipes.forEach((recipe) => {
+      if (recipe.componentes && Array.isArray(recipe.componentes)) {
+        this.filtradosComponentes[recipe.id] = recipe.componentes;
+      } else {
+        this.filtradosComponentes[recipe.id] = [];
+      }
+    });
+  }
 }
