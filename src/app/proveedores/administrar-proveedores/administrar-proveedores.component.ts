c93a57e02c7a50f8d72868c73ff10cb44f1aa3e1
KenayOG <angel1202a@gmail.com>
2024-07-30 15:05:24 -0600

    Eliminacion de datos estaticos, e invocacion del servicio para get proveedores

--- src/app/proveedores/administrar-proveedores/administrar-proveedores.component.ts
+++ src/app/proveedores/administrar-proveedores/administrar-proveedores.component.ts
@@ -1,42 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Provider } from '../../interfaces/provider';
+import { ProveedoresService } from '../../services/proveedores.service';
 
 @Component({
   selector: 'app-administrar-proveedores',
   templateUrl: './administrar-proveedores.component.html',
   styleUrl: './administrar-proveedores.component.css',
 })
-export class AdministrarProveedoresComponent {
-  providers: Provider[] = [
-    {
-      id: 1,
-      name: 'Sergio de Jesus Salazar Cabrera',
-      phone: '4792904456',
-      attendedBy: 'Jose Angel Ramirez Almeida',
-    },
-    {
-      id: 2,
-      name: 'Jose Angel Ramirez Almeida',
-      phone: '4792906049',
-      attendedBy: 'Sergio de Jesus Salazar Cabrera',
-    },
-    {
-      id: 3,
-      name: 'Adrian Dario Bravo Luna',
-      phone: '4793456799',
-      attendedBy: 'Jose Angel Ramirez Almeida',
-    },
-    {
-      id: 4,
-      name: 'Alexa Guerrero Lopez',
-      phone: '4792906321',
-      attendedBy: 'Adrian Dario Bravo Luna',
-    },
-    {
-      id: 5,
-      name: 'Angel Eduardo Juarez Alvizo',
-      phone: '4792203045',
-      attendedBy: 'Alexa Guerrero Lopez',
-    },
-  ];
+export class AdministrarProveedoresComponent implements OnInit {
+  providers: Provider[] = [];
+
+  constructor(private proveedoresService: ProveedoresService) {}
+  ngOnInit(): void {
+    this.proveedoresService.getProveedores().subscribe((data) => {
+      this.providers = data;
+    });
+  }
 }
