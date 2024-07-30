20d5ddbc5a54b3709bd6e89ef2b74603a8135149
KenayOG <angel1202a@gmail.com>
2024-07-29 22:04:28 -0600

    Metodo para invocar el get de recetas

---
+++ src/app/services/recetas.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../interfaces/recipe';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecetasService {
+  private _endPoint: string = environment.endPoint;
+  private apiUrl: string = this._endPoint + 'Receta';
+
+  constructor(private _http: HttpClient) {}
+
+  // Metodo para invocar el endpoint GET de recetas.
+  getRecetas(): Observable<Recipe[]> {
+    return this._http.get<Recipe[]>(`${this.apiUrl}/recetas`);
+  }
+}
