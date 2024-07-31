6a4fd642cf08754bd11fee59951df116c625acd2
KenayOG <angel1202a@gmail.com>
2024-07-31 02:28:06 -0600

    Archivo eliminado de merma interfaz

--- src/app/interfaces/decrease.ts
+++
@@ -1,9 +0,0 @@
-export interface Decrease {
-  id: number;
-  productDecreased: string;
-  lote: string;
-  quantityDecreased: number;
-  description: string;
-  dateDecrease: string;
-  decreasedBy: string;
-}
