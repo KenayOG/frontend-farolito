6097d3ba91585c7c533538894743eff81f3446d0
KenayOG <angel1202a@gmail.com>
2024-07-31 04:05:11 -0600

    Modulo de compras para el admin

---
+++ src/app/compras/compras.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComprasPorUsuarioComponent } from './compras-por-usuario/compras-por-usuario.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComprasPorUsuarioComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComprasModule { }
