c96c22182e20f83bfe72a900e3a37d1b4a7930ca
KenayOG <angel1202a@gmail.com>
2024-07-31 15:51:36 -0600

    Datos estaticos de productos eliminados, y gets para traer productos terminados

--- src/app/home/home-products/home-products.component.ts
+++ src/app/home/home-products/home-products.component.ts
@@ -1,6 +1,10 @@
 import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { NgbPopoverConfig, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
-import { Product } from '../../interfaces/product';
+import { LampInventory } from '../../interfaces/lamp-inventory';
+import { InventarioService } from '../../services/inventario.service';
+import { RecetasService } from '../../services/recetas.service';
+import { Recipe } from '../../interfaces/recipe';
+import { ComponenteRecipe } from '../../interfaces/component-recipe';
 
 @Component({
   selector: 'app-home-products',
@@ -9,178 +13,19 @@ import { Product } from '../../interfaces/product';
 })
 export class HomeProductsComponent {
   @ViewChildren('cartButton') cartbuttons!: QueryList<ElementRef>;
-  products: Product[] = [
-    {
-      id: 1,
-      name: 'Lampara Moderna',
-      price: 599.0,
-      image: 'lampara-moderna.webp',
-      components: ['Lampara Moderna 1', 'Lampara Moderna 2'],
-    },
-    {
-      id: 2,
-      name: 'Lampara de Mesa',
-      price: 119.5,
-      image: 'lampara-mesa.webp',
-      components: ['Lampara de Mesa 1', 'Lampara de Mesa 2'],
-    },
-    {
-      id: 3,
-      name: 'Lampara de Techo',
-      price: 340.5,
-      image: 'lampara-techo.webp',
-      components: ['Lampara de Techo 1', 'Lampara de Techo 2'],
-    },
-    {
-      id: 4,
-      name: 'Lamparas Colgantes',
-      price: 250.5,
-      image: 'lampara-colgante.webp',
-      components: ['Lampara Colgante 1', 'Lampara Colgante 2'],
-    },
-    {
-      id: 5,
-      name: 'Lampara Exteriores',
-      price: 399,
-      image: 'lampara-exteriores.webp',
-      components: ['Lampara Exteriores 1', 'Lampara Exteriores 2'],
-    },
-    {
-      id: 6,
-      name: 'Lampara de Piso',
-      price: 179.99,
-      image: 'lampara-pie.webp',
-      components: ['Lampara de Piso 1', 'Lampara de Piso 2'],
-    },
-    {
-      id: 7,
-      name: 'Lampara Moderna',
-      price: 599.0,
-      image: 'lampara-moderna.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 8,
-      name: 'Lampara de Mesa',
-      price: 119.5,
-      image: 'lampara-mesa.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 9,
-      name: 'Lampara de Techo',
-      price: 340.5,
-      image: 'lampara-techo.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 10,
-      name: 'Lamparas Colgantes',
-      price: 250.5,
-      image: 'lampara-colgante.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 11,
-      name: 'Lampara Exteriores',
-      price: 399,
-      image: 'lampara-exteriores.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 12,
-      name: 'Lampara de Piso',
-      price: 179.99,
-      image: 'lampara-pie.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 13,
-      name: 'Lampara Moderna',
-      price: 599.0,
-      image: 'lampara-moderna.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 14,
-      name: 'Lampara de Mesa',
-      price: 119.5,
-      image: 'lampara-mesa.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 15,
-      name: 'Lampara de Techo',
-      price: 340.5,
-      image: 'lampara-techo.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 16,
-      name: 'Lamparas Colgantes',
-      price: 250.5,
-      image: 'lampara-colgante.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 17,
-      name: 'Lampara Exteriores',
-      price: 399,
-      image: 'lampara-exteriores.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 18,
-      name: 'Lampara de Piso',
-      price: 179.99,
-      image: 'lampara-pie.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 19,
-      name: 'Lampara Moderna',
-      price: 599.0,
-      image: 'lampara-moderna.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 20,
-      name: 'Lampara de Mesa',
-      price: 119.5,
-      image: 'lampara-mesa.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 21,
-      name: 'Lampara de Techo',
-      price: 340.5,
-      image: 'lampara-techo.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 22,
-      name: 'Lamparas Colgantes',
-      price: 250.5,
-      image: 'lampara-colgante.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 23,
-      name: 'Lampara Exteriores',
-      price: 399,
-      image: 'lampara-exteriores.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-    {
-      id: 24,
-      name: 'Lampara de Piso',
-      price: 179.99,
-      image: 'lampara-pie.webp',
-      components: ['Detalle 1', 'Detalle 2'],
-    },
-  ];
+  products: LampInventory[] = [];
+  recipes: Recipe[] = [];
+  //cargando: boolean = true;
+  baseUrl: string = 'https://localhost:5000';
 
-  constructor(config: NgbPopoverConfig, private elementRef: ElementRef) {
+  constructor(
+    config: NgbPopoverConfig,
+    private elementRef: ElementRef,
+    private productosService: InventarioService,
+    private recetasService: RecetasService
+  ) {
+    this.obtenerProductos();
+    this.obtenerRecetas();
     config.placement = 'end';
     config.triggers = 'hover';
 
@@ -192,6 +37,51 @@ export class HomeProductsComponent {
       }
       return options;
     };
+  }
+
+  obtenerProductos() {
+    //this.cargando = true;
+    this.productosService.getInventarioLampara().subscribe({
+      next: (data) => {
+        this.products = data;
+        /* setTimeout(() => {
+          this.cargando = false;
+        }, 2000); */
+      },
+      error: (e) => {
+        console.log(e);
+        /* setTimeout(() => {
+          this.cargando = false;
+        }, 2000); */
+      },
+    });
+  }
+
+  obtenerRecetas() {
+    //this.cargando = true;
+    this.recetasService.getRecetas().subscribe({
+      next: (data) => {
+        this.recipes = data;
+        /* setTimeout(() => {
+          this.cargando = false;
+        }, 2000); */
+      },
+      error: (e) => {
+        console.log(e);
+        /* setTimeout(() => {
+          this.cargando = false;
+        }, 2000); */
+      },
+    });
+  }
+
+  getComponentesProductos(productId: number): ComponenteRecipe[] {
+    const recipe = this.recipes.find((r) => r.id === productId);
+    return recipe ? recipe.componentes : [];
+  }
+
+  getImagenProducto(imagePath: string): string {
+    return `${this.baseUrl}${imagePath}`;
   }
 
   animateCartButton(event: MouseEvent) {
