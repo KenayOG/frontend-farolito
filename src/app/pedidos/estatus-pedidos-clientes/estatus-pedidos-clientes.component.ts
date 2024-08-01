a4562c9eb9855e6440c1e2474215619982f9d142
KenayOG <angel1202a@gmail.com>
2024-07-31 20:51:27 -0600

    Datos estaticos removidos y codigo comentado

--- src/app/pedidos/estatus-pedidos-clientes/estatus-pedidos-clientes.component.ts
+++ src/app/pedidos/estatus-pedidos-clientes/estatus-pedidos-clientes.component.ts
@@ -1,52 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderProduct } from '../../interfaces/order-product';
 import { Recipe } from '../../interfaces/recipe';
+import { OrderCustomer } from '../../interfaces/customer-order';
 
 @Component({
   selector: 'app-estatus-pedidos-clientes',
   templateUrl: './estatus-pedidos-clientes.component.html',
   styleUrl: './estatus-pedidos-clientes.component.css',
 })
-export class EstatusPedidosClientesComponent implements OnInit {
-  orders: OrderProduct[] = [
-    {
-      id: 1,
-      nameProduct: 'Lampara Moderna',
-      price: 599.0,
-      quantity: 3,
-      image: 'assets/images/lampara-moderna.webp',
-      status: 'Enviado',
-    },
-    {
-      id: 2,
-      nameProduct: 'Lampara de Mesa',
-      price: 299.0,
-      quantity: 2,
-      image: 'assets/images/lampara-mesa.webp',
-      status: 'Finalizado',
-    },
-    {
-      id: 3,
-      nameProduct: 'Lampara Colgante',
-      price: 999.0,
-      quantity: 4,
-      image: 'assets/images/lampara-colgante.webp',
-      status: 'En proceso',
-    },
-    {
-      id: 4,
-      nameProduct: 'Lampara de Piso',
-      price: 899.0,
-      quantity: 6,
-      image: 'assets/images/lampara-pie.webp',
-      status: 'Sin Pagar',
-    },
-  ];
+export class EstatusPedidosClientesComponent implements OnInit{
+  orders: OrderCustomer[] = [];
 
-  filtroOrdenesEnviadas: OrderProduct[] = [];
-  filtroOrdenesEnproceso: OrderProduct[] = [];
-  filtroOrdenesSinpago: OrderProduct[] = [];
-  filtroOrdenesFinalizado: OrderProduct[] = [];
+  filtroOrdenesEnviadas: OrderCustomer[] = [];
+  filtroOrdenesEnproceso: OrderCustomer[] = [];
+  filtroOrdenesSinpago: OrderCustomer[] = [];
+  filtroOrdenesFinalizado: OrderCustomer[] = [];
   indexActivosTodo: number[] = [];
   indexActivosEnviadas: number[] = [];
   indexActivosEnproceso: number[] = [];
@@ -55,27 +23,26 @@ export class EstatusPedidosClientesComponent implements OnInit {
   recipes: Recipe[] = [];
 
   ngOnInit() {
-    /* Filtrar y mantener indices activos para apertura de acordiones */
     this.filtroOrdenesEnviadas = this.orders.filter(
-      (order) => order.status === 'Enviado'
+      (order) => order.estatus === 'Enviado'
     );
     this.indexActivosEnviadas = this.filtroOrdenesEnviadas.map(
       (_, index) => index
     );
     this.filtroOrdenesFinalizado = this.orders.filter(
-      (order) => order.status === 'Finalizado'
+      (order) => order.estatus === 'Finalizado'
     );
     this.indexActivosFinalizado = this.filtroOrdenesEnviadas.map(
       (_, index) => index
     );
     this.filtroOrdenesEnproceso = this.orders.filter(
-      (order) => order.status === 'En proceso'
+      (order) => order.estatus === 'En proceso'
     );
     this.indexActivosEnproceso = this.filtroOrdenesEnviadas.map(
       (_, index) => index
     );
     this.filtroOrdenesSinpago = this.orders.filter(
-      (order) => order.status === 'Sin Pagar'
+      (order) => order.estatus === 'Sin Pagar'
     );
     this.indexActivosSinpago = this.filtroOrdenesEnviadas.map(
       (_, index) => index
