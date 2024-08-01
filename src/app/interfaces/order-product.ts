a03b699b7d8618e13b8d6d9b06c14ce489c2f3cd
KenayOG <angel1202a@gmail.com>
2024-07-31 20:06:54 -0600

    Interfaz de ordenes de producto modificada

--- src/app/interfaces/order-product.ts
+++ src/app/interfaces/order-product.ts
@@ -1,8 +1,7 @@
-export interface OrderProduct{
-  id: number;
-  nameProduct: string;
-  price: number;
-  quantity: number;
-  image: string;
-  status: string;
+export interface OrderProduct {
+  recetaId: number;
+  inventarioId: number;
+  cantidad: number;
+  precioUnitario: number;
+  nombreProducto: string;
 }
