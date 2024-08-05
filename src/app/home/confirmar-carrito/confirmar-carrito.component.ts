64c0cb657200ef0dad487aec51fe8d6106d4eb71
Alexa Guerrero López <99861188+Guexa@users.noreply.github.com>
2024-08-04 21:59:44 -0600

    Eliminar del carrito implementado 100%

--- src/app/home/confirmar-carrito/confirmar-carrito.component.ts
+++ src/app/home/confirmar-carrito/confirmar-carrito.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
-import { Cart } from '../../interfaces/cart';
 import { CarritoService } from '../../services/carrito.service';
+import { Cart, CartRemove } from '../../interfaces/cart';
+import { LampInventory } from '../../interfaces/lamp-inventory';
+import { InventarioService } from '../../services/inventario.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-confirmar-carrito',
   templateUrl: './confirmar-carrito.component.html',
-  styleUrl: './confirmar-carrito.component.css',
+  styleUrls: ['./confirmar-carrito.component.css'],
 })
 export class ConfirmarCarritoComponent {
-  cartProducts: Cart[] = [];
 
-  constructor(private carritoService: CarritoService) {
+  cartProducts: Cart[] = [];
+  products: LampInventory[] = [];
+  baseUrl: string = 'https://localhost:5000';
+
+  constructor(
+    private carritoService: CarritoService,
+    private inventarioService: InventarioService,
+    private matSnackBar: MatSnackBar
+  ) {
     this.obtenerCarrito();
+    this.obtenerProductos();
   }
 
   obtenerCarrito() {
@@ -23,5 +34,56 @@ export class ConfirmarCarritoComponent {
         console.log(e);
       },
     });
+  }
+
+  obtenerProductos() {
+    this.inventarioService.getInventarioLampara().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
+
+  getImagenProducto(imagePath: string): string {
+    return `${this.baseUrl}${imagePath}`;
+  }
+
+  getPrice(productId: number): number {
+    const product = this.products.find(p => p.id === productId);
+    return product ? product.costo : 0;
+  }
+
+  calculateTotal(): number {
+    return this.cartProducts.reduce((total, cartItem) => {
+      return total + (cartItem.cantidad * this.getPrice(cartItem.lamparaId));
+    }, 0);
+  }
+
+  removeFromCart(productId: number) {
+    const removeRequest: CartRemove[] = [{ id: productId }];
+    this.carritoService.deleteCarrito(removeRequest).subscribe({
+      next: () => {
+        this.cartProducts = this.cartProducts.filter(item => item.lamparaId !== productId);
+        this.calculateTotal();
+        this.matSnackBar.open("Producto eliminado del carrito", 'Cerrar', {
+          duration: 5000,
+          horizontalPosition: 'center'
+        });
+      },
+      error: (err) => {
+        console.log(err);
+        this.matSnackBar.open('Ocurrió un problema: ' + (err.error.message || 'Desconocido'), 'Cerrar', {
+          duration: 5000,
+          horizontalPosition: 'center'
+        });
+      }
+    });
+  }
+
+  confirmarPago() {
+    console.log('Pago confirmado.');
   }
 }
--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Cart, CartRemove, CartRequest } from '../interfaces/cart';
 import { ResponsePosts } from '../interfaces/response-posts';
 
@@ -24,8 +24,10 @@ export class CarritoService {
   }
 
   // Método para eliminar del carrito
-  deleteCarrito(data: CartRemove): Observable<ResponsePosts> {
-    return this._http.delete<ResponsePosts>(`${this.apiUrl}/eliminar-del-carrito`, {
+  deleteCarrito(data: CartRemove[]): Observable<ResponsePosts> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this._http.request<ResponsePosts>('delete', `${this.apiUrl}/eliminar-del-carrito`, {
+      headers,
       body: data
     });
   }
