6336354d602b7d3812b07a386b9f19fe526222f1
Jahxx <j.mun.ev@outlook.com>
2024-10-04 17:54:05 -0600

    Ahora si terminé lo mío de vdd

--- src/app/autenticacion/sign-up/sign-up.component.ts
+++ src/app/autenticacion/sign-up/sign-up.component.ts
@@ -48,7 +48,7 @@ export class SignUpComponent implements OnInit {
     this.isUpperCase = /[A-Z]/.test(password);
     this.isLowerCase = /[a-z]/.test(password);
     this.isNumber = /\d/.test(password);
-    this.isSymbol = /[!@#$%^&*]/.test(password);
+    this.isSymbol = /[.*!@#$%^&,\-_']/.test(password);
     this.isMinLength = password.length >= 8;
   }
 
@@ -103,7 +103,7 @@ export class SignUpComponent implements OnInit {
     const hasLowerCase = /[a-z]/.test(password);
     const hasNumber = /\d/.test(password);
     const hasMinLength = password.length >= 8;
-    const hasSymbol = /[!@#$%^&*´,'.\-_+]/.test(password)
+    const hasSymbol = /[.*!@#$%^&,\-_']/.test(password)
 
     if (!hasUpperCase || !hasLowerCase || !hasNumber || !hasMinLength || !hasSymbol) {
       return {passwordWeak: true};
