da48eb1f25dc5565ff0d4059b109d3385e056c81
Salvador Muñoz <j.mun.ev@outlook.com>
2024-10-23 04:46:51 -0600

    Creación y corrección de características: 1. Recuperación de contraseña mediante enlace único por correo electrónico correctamente implementada. 2. Método seguro de código con tiempo de vencimiento (el token posee duración de 24hrs) 3. Añadida protección a la rutas "login, signup, reset-password, forgot-password". El usuario no podrá acceder a ellas si mantiene una sesión activa.

--- src/app/autenticacion/forgot-password/forgot-password.component.ts
+++ src/app/autenticacion/forgot-password/forgot-password.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {CustomerForgotten} from "../../interfaces/customer";
+import {ResponsePosts} from "../../interfaces/response-posts";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +11,52 @@ import { Component } from '@angular/core';
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
+  forgotPasswordForm: FormGroup;
+  isSubmitting: boolean = false;
 
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
+    this.forgotPasswordForm = formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.forgotPasswordForm.invalid) {
+      return;
+    }
+
+    this.isSubmitting = true;
+    const email = this.forgotPasswordForm.get('email')?.value;
+    const data: CustomerForgotten = {email};
+
+    this.authService.forgotPassword(data).subscribe({
+      next: (response: ResponsePosts) => {
+        if (response.isSuccess) {
+          this.snackBar.open('Correo enviado con éxito. Revisa tu bandeja de entrada.', 'Cerrar', {
+            duration: 5000,
+            panelClass: ['success-snackbar']
+          });
+        } else {
+          this.snackBar.open(response.message, 'Cerrar', {
+            duration: 5000,
+            panelClass: ['error-snackbar']
+          });
+        }
+        this.isSubmitting = false;
+      },
+      error: (error) => {
+        console.error('Ocurrió un error: ', error)
+        this.snackBar.open('Ocurrió un error. Inténtalo de nuevo más tarde.', 'Cerrar', {
+          duration: 5000,
+          panelClass: ['error-snackbar']
+        });
+        this.isSubmitting = false;
+      }
+    });
+  }
 }
